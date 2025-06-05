6b1d143146b679d3b46bd708e1f866d4c20d4256
wheecious <floudy@yandex.ru>
2025-06-05 14:57:53 +0400

    rm tests

--- tests/hamster/get-image.sh
+++
@@ -1,3 +0,0 @@
-get-image() {
-  ssh $PROJECT_USER@$PROJECT_HOST -c "kubectl describe pod -n dev $(kubectl get pods -n dev | grep hamster.*Running | awk '{print $1}') | grep Image | awk 'NR==1 {print $2}'"
-}
